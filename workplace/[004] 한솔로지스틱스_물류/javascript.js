094855d9ff417ca395e801a0bf4e849b4009c04e
jiyounghan <jyhan124@naver.com>
2023-04-02 23:42:58 +0900

    header - scroll() 적용, opacity 수정

--- workplace/[004] 한솔로지스틱스_물류/javascript.js
+++ workplace/[004] 한솔로지스틱스_물류/javascript.js
@@ -1,4 +1,12 @@
 $( document ).ready(function() {
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 0){
+            $('header').css('background','rgba(0,0,0,.7)')
+        }else{
+            $('header').css('background','transparent')
+        }
+    })
+
     $('.jq_slider1').slick({
         infinite: true,
         slidesToShow: 4,
