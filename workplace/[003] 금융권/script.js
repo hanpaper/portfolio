99181736aca7c528d245c4ab4a8b0f0961b03f9f
hanpaper <jyhan124@naver.com>
2023-05-16 18:49:26 +0900

    [금융권] 스크립트 파일 분리, 슬라이드 수정(bxslider, swiper)

--- workplace/[003] 금융권/script.js
+++ workplace/[003] 금융권/script.js
@@ -1,8 +1,42 @@
 $( document ).ready(function() {
 
+    //top button
     $('.quickmenu').click(function(){
         window.scrollTo(0,0)
     })
     
-    
+    //slider
+    var swiper = new Swiper(".swiper_type1", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+  
+          
+          var swiper = new Swiper(".swiper_type2", {
+          slidesPerView: 6,
+          navigation: {
+              nextEl: ".btn_next_type2",
+              prevEl: ".btn_prev_type2",
+          }
+          });
+
+
+
+          $(".bxslider_type1").bxSlider({
+            mode: 'vertical',
+            pager: false,
+
+            auto:true,
+            pause:2500,
+          });
+  
+          
     });
