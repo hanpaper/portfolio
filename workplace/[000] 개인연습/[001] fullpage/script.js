800d9d703cdf61422eea238edc51cdd6257fcba7
hanpaper <jyhan124@naver.com>
2023-05-18 23:43:42 +0900

    fullpage js 연습

---
+++ workplace/[000] 개인연습/[001] fullpage/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		//options here
+        anchors: ['section1', 'section2', 'section3','section4'],
+        navigation:true,
+        navigationPosition:'right',
+        sectionsColor : ['#ccc' , '#bbb' , '#eee' , '#fff'],
+        
+        afterLoad: function(anchorLink, index){
+            console.log('현재 섹션 번호는?' + index)
+        },
+        
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+            console.log('현재 슬라이드 번호는?' + slideIndex)
+        }
+	});
+    
+});
