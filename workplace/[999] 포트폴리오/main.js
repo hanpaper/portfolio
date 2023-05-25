0accda9ccef809da41e2fa03158fe80b60e70aaa
hanpaper <jyhan124@naver.com>
2023-05-25 13:52:53 +0900

    [포폴] visual/contact section ani추가, work section 레이아웃 변경

--- workplace/[999] 포트폴리오/main.js
+++ workplace/[999] 포트폴리오/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+// header
 var header = document.querySelector('header')
 setInterval(function(){
     if(pageYOffset > 0){
@@ -12,14 +12,94 @@ setInterval(function(){
 
 
 
-// fullpage 기능
-$('#fullpage').fullpage({
-    //options here
-    anchors: ['section1', 'section2', 'section3','section4'],
-    navigation:true,
-    navigationPosition:'right',
-
-
+// scroll 기능 제거
+$('main').on('scroll touchmove mousewheel', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    return false;
 });
+
+// scroll 기능 풀기
+$('main').off('scroll touchmove mousewheel');
+
+// scroll 기능 제거 (function으로 빼기)
+// function scroll_on() {
+//     $('main').on('scroll touchmove mousewheel', function(e) {
+//         e.preventDefault();
+//         e.stopPropagation();
+//         return false;
+//     });
+// };
+
+
+
+
+// offset()측정
+setInterval(function(){
+    console.log('맨위에서 section까지의 거리 :'+$('.works').offset().top);
+    console.log('스크롤된 정도:'+$(window).scrollTop());
+},2000);
+
+
+
+
+
+
+// infinite slider 기능
+var bannerLeft=0;
+        var first=1;
+        var last;
+        var sliderNum=0;
+        var slider = $(".infSlider");
+        var $first;
+        var $last;
+
+        slider.each(function(){   // 20px 간격으로 배너 처음 위치 시킴
+            $(this).css("left",bannerLeft);
+            bannerLeft += $(this).width()+120;
+            $(this).attr("id", "banner"+(++sliderNum));  // slider에 id 속성 추가
+        });
+
+
+        if( sliderNum > 3){          //배너 3개 이상일 때, 작동시켜라
+
+            last = sliderNum;
+
+            setInterval(function() {
+                slider.each(function(){
+                    $(this).css("left", $(this).position().left-1); // 1px씩 왼쪽으로 이동
+                });
+
+                $first = $("#banner"+first);
+                $last = $("#banner"+last);
+                if($first.position().left < -700) {    // 제일 앞에 배너 제일 뒤로 옮김
+                    $first.css("left", $last.position().left + $last.width()+120);
+                    first++; // +1한 뒤, $first = $("#banner"+first);에 대입 (banner1이 뒤로가면서 banner2가 $first가 됨)
+                    last++;
+                    if(last > sliderNum) { last=1; }   
+                    if(first > sliderNum) { first=1; }
+                }
+            }, 10);     //속도를 조정          
+
+        };
+
+
+
+
+// fullpage 기능
+// $('#fullpage').fullpage({
+//     //options here
+//     anchors: ['section1', 'section2', 'section3','section4'],
+// 
+//     
+//     afterLoad: function(anchorLink, index){
+//         console.log('현재 섹션 번호는?' + index)
+//     },
+//     
+//     afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){
+//         console.log('현재 슬라이드 번호는?' + slideIndex)
+//     },
+// 
+// });
 
 });
