026ce41b26aff61b24ded6abdb63084bd9763e48
hanpaper <jyhan124@naver.com>
2023-06-08 12:35:33 +0900

    [포폴] 텍스트 아웃라인/works 링크연결/다운로드 파일 연결/blog api 연결

--- workplace/[999] 포트폴리오/main.js
+++ workplace/[999] 포트폴리오/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
+
+    $.ajax({
+        method: "GET",
+        url: "https://www.tistory.com/apis/post/list?access_token=ca6a73de64c77ff2b3561c3b56506003_5e001e572ee39e409fd2fa36c4fbc3d9&output=xml&blogName=chjy0124&page=1",
+      })
+        .done(function( msg ) {
+            // console.log(msg);
+
+            const blogTitle = document.getElementsByClassName('blogTitle')
+            const blogDate = document.getElementsByClassName('blogDate')
+            const blogUrl = document.getElementsByClassName('blogUrl')
+            for(i=0 ; i < blogTitle.length ; i++){
+                blogTitle[i].innerHTML = msg.getElementsByTagName('title')[i].innerHTML;
+                blogDate[i].innerHTML = msg.getElementsByTagName('date')[i].innerHTML;
+                blogUrl[i].href = msg.getElementsByTagName('postUrl')[i].innerHTML;
+            }
+        });
     
 // scroll 기능
     const body = document.getElementsByTagName('body')[0];
     const main = document.getElementsByTagName('main')[0];
-    const sec = document.getElementsByClassName('section');
+    var header = document.querySelector('header');
+    const part1 = document.getElementById('part1');
+    const part2 = document.getElementById('part2');
+    const sec = part2.getElementsByClassName('section');
     const works = document.getElementsByClassName('works')[0];
     const contBox = works.getElementsByClassName('contentBox')[0];
     const scrollBox = document.getElementById('scrollBox');
@@ -13,51 +33,54 @@ $(document).ready(function() {
 
     body.addEventListener('wheel', function(e){
         
-        if(works.offsetTop*-1 + 'px' === main.style.top && a < 0 && a > -900 ||
-        works.offsetTop*-1 + 'px' === main.style.top && a === 0 && e.deltaY > 0 || 
-        works.offsetTop*-1 + 'px' === main.style.top && a === -900 && e.deltaY < 0)
+        if(works.offsetTop*-1 + 'px' === part2.style.top && a < 0 && a > -900 ||
+        works.offsetTop*-1 + 'px' === part2.style.top && a === 0 && e.deltaY > 0 || 
+        works.offsetTop*-1 + 'px' === part2.style.top && a === -900 && e.deltaY < 0)
         {
             scrollRow(e.deltaY);
             return;
-        }else if(works.offsetTop*-1 + 'px' === main.style.top && a === 0 && e.deltaY < 0 || 
-        works.offsetTop*-1 + 'px' === main.style.top && a === -1200 && e.deltaY > 0){
+        }else if(works.offsetTop*-1 + 'px' === part2.style.top && a === 0 && e.deltaY < 0 || 
+        works.offsetTop*-1 + 'px' === part2.style.top && a === -1200 && e.deltaY > 0){
             move_slider(e.deltaY);
         };
+        
         move_slider(e.deltaY);
+        move_typo(e.deltaY);
         
         console.log('마우스:' + e.deltaY);
     });
-
+    
 
     function move_slider(amount){
         k += amount;
+
+        console.log('k값:' + k);
         
-        for(i=1 ; i < sec.length ; i++){ // 섹션구간정지
-            if( sec[i].offsetTop - 150 < k && k < sec[i].offsetTop ||
-            sec[i].offsetTop < k && k < sec[i].offsetTop + 150 ){
-                k = sec[i].offsetTop ;
-            }
+        for(i=0 ; i < sec.length ; i++){ // 섹션구간정지
+            
+            if( sec[i].offsetTop - 150 < k && k < sec[i].offsetTop && amount > 0 ||
+            sec[i].offsetTop < k && k < sec[i].offsetTop + 150 && amount < 0 ){
+                part2.style.top = -sec[i].offsetTop + 'px';
+                k = sec[i].offsetTop;
+            };
         };
 
         if(k < 0){ // 시작점
             k=0;
             return;
         }
-        else if( k > sec[sec.length-1].offsetTop + sec[sec.length-1].offsetHeight - window.innerHeight){ // 종료점
-            k = sec[sec.length-1].offsetTop + sec[sec.length-1].offsetHeight - window.innerHeight;
+        else if( k > sec[sec.length-1].offsetTop + sec[sec.length-1].offsetHeight - window.innerHeight ){ // 종료점
+            k = sec[sec.length-1].offsetTop + sec[sec.length-1].offsetHeight - window.innerHeight ;
             return;
         };
 
-        main.style.top = k*-1 + 'px';
-        console.log('k값:' + k)
+        part2.style.top = - k + 'px';
+        console.log('part2 top값:' + sec[1].offsetTop);
     };
 
 
-    setInterval(function(){
-        scrollBox.style.paddingLeft = contBox.offsetLeft + 30 + 'px';
-    });
 
-    function scrollRow(amount){
+     function scrollRow(amount){
         a -= amount;        
         if(a > 0){
             a = 0;
@@ -71,20 +94,49 @@ $(document).ready(function() {
 
 
 
-    // header
-    var header = document.querySelector('header')
-    setInterval(function(){
-        if(k > 0){
-            header.setAttribute('class','scroll')
-        }else{
-            header.removeAttribute('class','scroll')
+
+
+    // typo position
+    const typoIam = document.getElementById('typo_iam');
+    const typoStart = document.getElementById('typo_start');
+
+    
+    sec[0].style.height = 'calc(100% + '+typoStart.offsetWidth+'px)'
+    typoIam.style.left = 'calc(50% + '+typoIam.offsetHeight+'px)';
+    typoIam.style.bottom = 'calc(100% - 274px)';
+
+    typoStart.style.right = - typoStart.offsetWidth + typoStart.offsetHeight/2 + 'px';
+    
+
+
+    let m = 274
+    
+
+    function move_typo(amount){
+        m += amount 
+        
+        
+        if(m < 274){
+            m=274;
+            return;
         }
+        typoIam.style.bottom = 'calc(100% - '+m+'px)';
+        
+        return;
+    };
+
+
+
+    // header
+    setInterval(function(){
+
+    scrollBox.style.paddingLeft = contBox.offsetLeft + 30 + 'px';
     });
 
 
 
 
-
+    
     // scroll 기능 제거
     // $('main').on('scroll touchmove mousewheel', function(e) {
     //     e.preventDefault();
@@ -116,12 +168,27 @@ $(document).ready(function() {
 
 
     // works datail window
-    document.getElementById('hansol').addEventListener('click', function(){
-        document.getElementById('detail_hansol').style.display = 'block';
-    });
-    document.getElementById('btn_close').addEventListener('click', function(){
-        document.getElementById('detail_hansol').style.display = 'none';
-    });
+    // document.getElementById('hansol').addEventListener('click', function(){
+    //     document.getElementById('detailWin').style.opacity = '1'
+    //     document.getElementById('detailWin').style.pointerEvents = 'all';
+    //     document.getElementById('detailHansol').style.right = '0px';
+    // });
+    // document.getElementById('btn_close').addEventListener('click', function(){
+    //     document.getElementById('detailWin').style.opacity = '0'
+    //     document.getElementById('detailWin').style.pointerEvents = 'none';
+    //     document.getElementById('detailHansol').style.right = '-100%';
+    // });
+// 
+    // document.getElementById('bank').addEventListener('click', function(){
+    //     document.getElementById('detailWin').style.opacity = '1'
+    //     document.getElementById('detailWin').style.pointerEvents = 'all';
+    //     document.getElementById('detailBank').style.right = '0px';
+    // });
+    // document.getElementById('btn_close').addEventListener('click', function(){
+    //     document.getElementById('detailWin').style.opacity = '0'
+    //     document.getElementById('detailWin').style.pointerEvents = 'none';
+    //     document.getElementById('detailBank').style.right = '-100%';
+    // });
 
 
 
