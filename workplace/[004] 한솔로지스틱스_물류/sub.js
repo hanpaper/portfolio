e19bdeca41b3f352a0dff49fbb6da02fc20c297c
hanpaper <jyhan124@naver.com>
2023-06-15 18:54:05 +0900

    [한솔로지스틱스] 반응형/웹 접근성 관련 내용 수정

--- workplace/[004] 한솔로지스틱스_물류/sub.js
+++ workplace/[004] 한솔로지스틱스_물류/sub.js
@@ -1,6 +1,19 @@
 $( document ).ready(function() {
 
-    // snb-menu
+// header
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 0){
+                $('header').css('background','rgba(0,0,0,.7)')
+            }else{
+                $('header').css('background','transparent')
+            }
+        })
+    
+        $('.btn_up').click(function(){
+            window.scrollTo(0, 0)
+        })
+
+// snb-menu
     const snb_menu = document.querySelectorAll('.snb_menu button')
     for(i=0; i<snb_menu.length; i++){
         snb_menu[i].addEventListener('click', function(){
