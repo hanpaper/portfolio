7234a3b3a94248d0eab57e9fa03830222f8089d6
hanpaper <jyhan124@naver.com>
2023-06-26 17:55:37 +0900

    [한솔로지스틱스] 클래스 BEM방식 적용

--- workplace/[004] 한솔로지스틱스_물류/main.js
+++ workplace/[004] 한솔로지스틱스_물류/main.js
@@ -13,9 +13,44 @@ $( document ).ready(function() {
         window.scrollTo(0, 0)
     })
 
+// header - gnb
+const body = document.getElementsByTagName('body')[0];
+const header = document.getElementsByTagName('header')[0];
+const header_logo = header.getElementsByClassName('logo')[0];
+const header_nav = header.getElementsByTagName('nav')[0];
+const header_lang = document.getElementsByClassName('header__lang')[0];
+const sub_gnb = document.getElementsByClassName('sub-gnb')[0];
+const header_menu = document.getElementsByClassName('header__menu')[0];
+
+header_menu.addEventListener('click',function(){
+    header_menu.classList.toggle('active');
+    header_lang.classList.toggle('header__menu-active');
+    sub_gnb.classList.toggle('active');
+    
+    if( sub_gnb.classList.contains('active') === true ){
+        body.style.overflow = 'hidden'
+        header_logo.style.display = 'none'
+        header_nav.style.display = 'none'
+    }else{
+        body.style.overflow = 'auto'
+        header_logo.style.display = 'inline-block'
+        header_nav.style.display = 'inline'
+    }
+});
+
+const btn_submenu = document.querySelectorAll('.btn-submenu')
+
+for(i=0 ; i < btn_submenu.length ; i++){
+    btn_submenu[i].addEventListener('click', function(){
+        this.parentNode.classList.toggle('active');
+        console.log(this.parentNode);
+        let a = this.parentNode;
+    });
+}
+
 
 // slick 자동재생 슬라이드
-    $('.fade').slick({
+    $('.slide-slick1').slick({
         dots: true,
         infinite: true,
         autoplay: true,
@@ -34,21 +69,36 @@ $( document ).ready(function() {
     }
     
 // slick 무한루프 슬라이드
-    $('.slick-slider1').slick({
+    $('.slide-slick2').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        dots: true,
+        dots: false,
         responsive: 
-        [{ breakpoint: 1029, // 반응형: 1029px이하
-                settings: { slidesToShow:3
+        [{breakpoint: 1640, // 반응형: 1640px이하
+                 settings: { arrows: false,
+                              dots: true
+
+                }
+            },
+            {breakpoint: 1029, // 반응형: 1029px이하
+                settings: { slidesToShow:3,
+                    arrows: false,
+                              dots: true
+
                 }
             },
             { breakpoint: 769, // 반응형:769px이하
-                settings: { slidesToShow:2
+                settings: { slidesToShow:2,
+                    arrows: false,
+                              dots: true
+
                 }
             },
             { breakpoint: 639, // 반응형:639px이하
-                settings: { slidesToShow:1
+                settings: { slidesToShow:1,
+                              arrows: false,
+                              dots: true
+
                 }
             },
         ]
