59c9e4e60dff1e7f3b3c6bee56c596a7d3748da7
hanpaper <jyhan124@naver.com>
2023-07-01 21:21:54 +0900

    [금융권] 클래스 및 코드 정리

--- workplace/[003] 금융권/script.js
+++ workplace/[003] 금융권/script.js
@@ -1,69 +1,78 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-    //top button
-    $('.quickmenu').click(function(){
-        window.scrollTo(0,0)
-    })
-    
+  //top button
+  const btn_up = document.getElementsByClassName('btn-up')[0];
 
-    //slider
-    var swiper = new Swiper(".swiper_type1", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-      });
+  btn_up.addEventListener('click', function(){
+    window.scrollTo(0, 0)
+  });
+
+
+  //slider
+  var swiper = new Swiper(".slide-swiper__type1", {
+    spaceBetween: 0, // 슬라이드 사이 간격
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var swiper = new Swiper(".slide-swiper__type2", {
+    slidesPerView: 6,
+    navigation: {
+      nextEl: ".btn-next__type2",
+      prevEl: ".btn-prev__type2",
+    }
+  });
   
-          
-          var swiper = new Swiper(".swiper_type2", {
-          slidesPerView: 6,
-          navigation: {
-              nextEl: ".btn_next_type2",
-              prevEl: ".btn_prev_type2",
-          }
-          });
 
+  var swiper = new Swiper(".slide-swiper__type3", {
+    direction: "vertical",
+    navigation: {
+      nextEl: ".btn-next__type3",
+      prevEl: ".btn-prev__type3",
+    }
+  });
 
-
-          $(".bxslider_type1").bxSlider({
-            mode: 'vertical',
-            pager: false,
-
-            auto:true,
-            pause:2500,
-          });
   
-          // menubar - popup
-        $('.menu .btn_search').click(function(){
-            if($('.popup').css("display") === 'none'){
-                $('.popup').css("display","block");
-                $('.popup_search').css("display","block");
-                $('body').css("overflow","hidden")
-            }else{
-                $('.popup').css("display","none");
-                $('.popup_search').css("display","none");
-                $('body').css("overflow","auto")
-            }
-        });
-        
+  // menubar
+  const body = document.getElementsByTagName('body')[0];
+  const btn_search = document.querySelectorAll('.menu .btn-search')[0];
+  const btn_menu = document.querySelectorAll('.menu .btn-menu')[0];
+  const popup = document.querySelectorAll('.popup')[0];
+  const searchBox = document.querySelectorAll('.search-box')[0];
+  const megamenu = document.querySelectorAll('.megamenu')[0];
+  const btn_close = document.querySelectorAll('.popup .btn-close');
+  const popupState = popup.querySelectorAll('active');
 
-        $('.menu .btn_menu').click(function(){
-                $('.popup').css("display","block");
-                $('.popup_magamenu').css("display","block");
-                $('body').css("overflow","hidden")
-        });
+  btn_search.addEventListener('click', function () {
+    popup.classList.add('active');
+    searchBox.classList.add('active');
+    body.classList.add('overflow-hidden')
+  });
 
+  btn_menu.addEventListener('click', function () {
+    popup.classList.add('active');
+    megamenu.classList.add('active');
+    searchBox.classList.remove('active');
+    body.classList.add('overflow-hidden')
 
-        $('.popup .btn_close').click(function(){
-                $('.popup').css("display","none");
-                $('.popup_search').css("display","none");
-                $('.popup_magamenu').css("display","none");
-                $('body').css("overflow","auto")
-            })
+  });
+
+  for (i = 0; i < btn_close.length; i++) {
+    btn_close[i].addEventListener('click', function () {
+      popup.classList.remove('active');
+      searchBox.classList.remove('active');
+      megamenu.classList.remove('active');
+      body.classList.remove('overflow-hidden')
     });
+  }
+
+});
